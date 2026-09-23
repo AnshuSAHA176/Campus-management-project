9ceb6226a105ace2ea5f827e5403d6efe3a53a1e
Anshu Saha <anshusahaa62@gmail.com>
2026-09-23 10:42:20 +0530

    user model done

--- src/apps/account/models.py
+++ src/apps/account/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 import uuid
+from .custom_manager import CustomeUsermanager
+from django.contrib.auth.models import PermissionsMixin
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser,PermissionsMixin):
     class RoleChoices(models.TextChoices):
         STUDENT=('student','student')
         Teacher=('teacher','Teacher')
         ADMIN=('admin','Admin')
         HOD=('hod','HOD')
+
+ 
+    objects  = CustomeUsermanager()
     id = models.UUIDField(primary_key=True,default=uuid.uuid4)
     user_name = None
-    email = models.EmailField()
+    email = models.EmailField(
+
+         verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
     role = models.CharField(
         max_length=200,
         choices=RoleChoices.choices,
@@ -33,6 +43,6 @@ class User(AbstractBaseUser):
     
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
-    
+
     REQUIRED_FIELDS = []
 
